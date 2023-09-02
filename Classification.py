8009aa7a7f40d7c9303358c44f8f945fd782dd17
Long-Sa <samy.bonet23@gmail.com>
2023-09-02 23:51:35 +0200

    changes

--- Classification.py
+++ Classification.py
@@ -6,22 +6,6 @@ import tensorflow_hub as hub
 import numpy as np
 
 
-def feature_USE_fct(sentences, b_size) :
-    
-    embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
-    
-    batch_size = b_size
-
-    for step in range(len(sentences)//batch_size) :
-        idx = step*batch_size
-        feat = embed(sentences[idx:idx+batch_size])
-
-        if step ==0 :
-            features = feat
-        else :
-            features = np.concatenate((features,feat))
-
-    return features
 
 def classification_(text):
     # load the model from server
@@ -44,10 +28,10 @@ def classification_USE(text):
 
     loaded_model = pickle.load(open('model_USE.json', 'rb'))
     loaded_binerizer = pickle.load(open('binarizer.json', 'rb'))
+    embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
     
-    features_USE = feature_USE_fct(text, 1)
+    features_USE = embed(text)
     
-   
     predict = loaded_model.predict_proba(features_USE) > 0.3
     predic_input = predict.astype(int)
     tags_pred = loaded_binerizer.inverse_transform(predic_input)
