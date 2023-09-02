a6b45fbc4ac28a70108cb6b5f36b513de6040a7b
Long-Sa <samy.bonet23@gmail.com>
2023-09-02 21:21:24 +0200

    changes models

--- API.py
+++ API.py
@@ -5,15 +5,32 @@ from Preprocessing import preprocessing
 
 def run():
     st.title("Prédiction des tags pour les questions stackoverflow")
-    st.header('Cette application sert à proposer des tags pertinents les posts stackoverflow')
-    titre = st.text_area('Donner un titre à votre poste')
-    texte = st.text_area('Expliquer votre interrogation')
+    st.subheader('Cette application sert à proposer des tags pertinents les posts stackoverflow')
+    add_selectbox = st.sidebar.selectbox("How would you like to predict?", ("Word2Vec et Regression Logistique", "TFIDF et SVC"))
     
-    body = titre + texte 
     
-    if st.button("Predict"):
-        output = classification_(preprocessing((body)))
-        st.success(f" Les tags les plus pertinents sont {output}")
-        st.balloons()
+    if add_selectbox == "Word2Vec et Regression Logistique":
+        
+        titre = st.text_area('Donner un titre à votre poste')
+        texte = st.text_area('Expliquer votre interrogation')
+        body = titre + texte
+      
+        if st.button("Predict"):
+            output = classification_(preprocessing((body)))
+            st.success(f" Les tags les plus pertinents sont {output}")
+            st.balloons()
+            
+    elif add_selectbox == "TFIDF et SVC":
+        
+        titre = st.text_area('Donner un titre à votre poste')
+        texte = st.text_area('Expliquer votre interrogation')
+        body = titre + texte
+        
+        if st.button("Predict"):
+            output = classification_(preprocessing((body)))
+            st.success(f" Les tags les plus pertinents sont {output}")
+            st.balloons()
+             
+            
     
 run()  
--- Classification.py
+++ Classification.py
@@ -3,17 +3,14 @@ from itertools import chain
 import pickle
 import sklearn
 
-embedding_path = r'model.json'
-filepath = r'binarizer.json'
 
-
-def classification_(embedding):
+def classification_(text):
     # load the model from server
 
     loaded_model = pickle.load(open('model.json', 'rb'))
     loaded_binerizer = pickle.load(open('binarizer.json', 'rb'))
     
-    predict = loaded_model.predict(embedding) > 0.3
+    predict = loaded_model.predict_proba(text) > 0.3
     predic_input = predict.astype(int)
     tags_pred = loaded_binerizer.inverse_transform(predic_input)
     
@@ -22,3 +19,20 @@ def classification_(embedding):
 
     return Output
 
+
+def classification_word2vec(text):
+    # load the model from server
+
+    loaded_model = pickle.load(open('model_wordvec.json', 'rb'))
+    text_to = pickle.load(open('embed_model.json', 'rb'))
+    loaded_binerizer = pickle.load(open('binarizer.json', 'rb'))
+    
+    embedding = text_to.predict(text)
+    predict = loaded_model.predict_proba(embedding) > 0.3
+    predic_input = predict.astype(int)
+    tags_pred = loaded_binerizer.inverse_transform(predic_input)
+    
+    tags = sorted(filter(None, set(tags_pred)))
+    Output = list(set(map(''.join, chain.from_iterable(tags))))
+
+    return Output
