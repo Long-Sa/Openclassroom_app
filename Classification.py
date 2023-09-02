f5526777986c2ca42e814ce70ec86bc4b604146a
Long-Sa <samy.bonet23@gmail.com>
2023-09-02 21:49:03 +0200

    message

--- Classification.py
+++ Classification.py
@@ -4,6 +4,22 @@ import pickle
 import sklearn
 
 
+def feature_USE_fct(sentences, b_size) :
+    batch_size = b_size
+    time1 = time.time()
+
+    for step in range(len(sentences)//batch_size) :
+        idx = step*batch_size
+        feat = embed(sentences[idx:idx+batch_size])
+
+        if step ==0 :
+            features = feat
+        else :
+            features = np.concatenate((features,feat))
+
+    time2 = np.round(time.time() - time1,0)
+    return features
+
 def classification_(text):
     # load the model from server
 
@@ -23,12 +39,15 @@ def classification_(text):
 def classification_word2vec(text):
     # load the model from server
 
-    loaded_model = pickle.load(open('model_wordvec.json', 'rb'))
-    text_to = pickle.load(open('embed_model.json', 'rb'))
+    loaded_model = pickle.load(open('model_worUSE.json', 'rb'))
     loaded_binerizer = pickle.load(open('binarizer.json', 'rb'))
     
-    embedding = text_to.predict(text)
-    predict = loaded_model.predict_proba(embedding) > 0.3
+    
+    embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+    features_USE = feature_USE_fct(text.to_list(), 1)
+    
+   
+    predict = loaded_model.predict_proba(features_USE) > 0.3
     predic_input = predict.astype(int)
     tags_pred = loaded_binerizer.inverse_transform(predic_input)
     
